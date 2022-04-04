23722216cac98dd97541f9db8554f45d371ea097
evsvmx <evsv.mx@gmail.com>
2022-04-04 15:02:40 +0300

    fix errors

--- views/site/finish.php
+++ views/site/finish.php
@@ -1,18 +1,5 @@
 <?php
 
-/** @var yii\web\View $this */
-
-/** @var FromFifthForm $form */
-
-/** @var Districts $district */
-
-use app\forms\FromFifthForm;
-use app\models\Districts;
-
-$fromFourthForm = $form->fromFourthForm;
-$fromThirdForm = $fromFourthForm->fromThirdForm;
-$secondForm = $fromThirdForm->secondForm;
-
 $this->title = 'Спасибо';
 ?>
 <div class="mt-5" style="min-height: 100%">
@@ -20,8 +7,3 @@ $this->title = 'Спасибо';
         Благодарим вас за ответы, мы свяжемся с вами!
     </h2>
 </div>
-
-
-<script>
-</script>
-
