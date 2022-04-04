edb39d93d37a2a0147ab46938590ad3ddc98f20c
evsvmx <evsv.mx@gmail.com>
2022-04-04 16:20:41 +0300

    replaced image

--- views/site/first.php
+++ views/site/first.php
@@ -2,15 +2,19 @@
 
 /** @var yii\web\View $this */
 
-/** @var \app\forms\FirstForm $form */
+/** @var FirstForm $form */
+
+use app\forms\FirstForm;
+use yii\helpers\Url;
+use yii\widgets\ActiveForm;
 
 $this->title = 'Анкета';
 
 ?>
 <div class="d-flex flex-column" style="min-height: 100%">
-    <?php $activeForm = \yii\widgets\ActiveForm::begin([
+    <?php $activeForm = ActiveForm::begin([
         'method' => 'POST',
-        'action' => \yii\helpers\Url::to(['/2']),
+        'action' => Url::to(['/2']),
         'options' => [
             'class' => 'd-flex flex-column',
             'id' => 'form',
@@ -18,7 +22,7 @@ $this->title = 'Анкета';
         ]
     ]) ?>
 
-    <img src="/images/image 1.jpg" alt="" class="mb-3">
+    <img src="/images/9a609fc952194b5676d66842f8a9d2ce.jpg" alt="" class="mb-3">
     <p class="font-weight-bold text-center mb-4" style="font-size: 19px">
         Чтобы записаться на уборку 2022г. заполните форму ниже
     </p>
@@ -27,10 +31,10 @@ $this->title = 'Анкета';
 
     <?= $activeForm->field($form, 'type')
         ->label(false)
-        ->dropDownList(\app\forms\FirstForm::$types, ['prompt' => 'Выбрать', 'class' => 'form-control bg-gray']) ?>
+        ->dropDownList(FirstForm::$types, ['prompt' => 'Выбрать', 'class' => 'form-control bg-gray']) ?>
 
 
-    <?php \yii\widgets\ActiveForm::end() ?>
+    <?php ActiveForm::end() ?>
     <button
             type="submit"
             class="btn btn-gray w-100 mb-5"
